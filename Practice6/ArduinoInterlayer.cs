9826166af2dae8f9280689fec026ced50a6de64b
WizMe-M <enderman.2016@mail.ru>
2021-12-01 16:49:32 +0300

    Initial commit

---
+++ Practice6/ArduinoInterlayer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using RJCP.IO.Ports;
+
+namespace Practice6
+{
+    public class ArduinoInterlayer
+    {
+        public Action InvasionHappened;
+        public readonly int SubstationCount;
+        private readonly SerialPortStream _port;
+        
+        public ArduinoInterlayer()
+        {
+            _port = new SerialPortStream();
+            _port.BaudRate = 9600;
+            _port.DataBits = 8;
+            _port.Parity = Parity.None;
+            _port.StopBits = StopBits.One;
+            _port.Encoding = Encoding.ASCII;
+            _port.ReadTimeout = 1000;
+            _port.WriteTimeout = 1000;
+            _port.DtrEnable = true;
+            _port.RtsEnable = true;
+            
+            SubstationCount = 2;
+        }
+
+        public bool IsArduinoConnected => _port.IsOpen;
+
+        public static IEnumerable<string> GetPortNames() => SerialPortStream.GetPortNames();
+
+        public void ConnectToPort(string portName)
+        {
+            _port.PortName = portName;
+            _port.DataReceived += OnDataReceived;
+            _port.ErrorReceived += OnErrorReceived;
+            _port.Open();
+        }
+
+        public void DisconnectFromPort()
+        {
+            _port.DataReceived -= OnDataReceived;
+            _port.ErrorReceived -= OnErrorReceived;
+            _port.Close();
+        }
+
+        public void LightLed(int substationIndex)
+        {
+            _port.Write("light" + substationIndex);
+        }
+
+        public void TurnOffLed(int substationIndex)
+        {
+            _port.Write("turnoff" + substationIndex);
+        }
+
+        public void EnableSignalling()
+        {
+            _port.Write("enableSignal");
+        }
+
+        public void DisableSignalling()
+        {
+            _port.Write("disableSignal");
+        }
+
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs _)
+        {
+            var port = (SerialPortStream)sender;
+            var data = port.ReadLine();
+            data = data?.Trim().ToLower();
+            Debug.WriteLine(data, "port output");
+
+            if (data == "invasion")
+            {
+                InvasionHappened?.Invoke();
+            }
+        }
+
+        private void OnErrorReceived(object sender, SerialErrorReceivedEventArgs _)
+        {
+            var port = (SerialPortStream)sender;
+            var error = port.ReadLine().Trim().ToLower();
+            Debug.WriteLine(error, "port errors");
+        }
+    }
+}
---
+++ Practice6/MainWindow.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Practice6
+{
+    public partial class MainWindow
+    {
+        private readonly ArduinoInterlayer _arduino;
+
+        public MainWindow()
+        {
+            _arduino = new ArduinoInterlayer();
+            InitializeComponent();
+        }
+
+        #region UserInterfaceMethods
+
+        private void MainWindow_OnClosed(object sender, EventArgs e)
+        {
+            _arduino.DisconnectFromPort();
+        }
+
+        private void FindPortsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var portNames = ArduinoInterlayer.GetPortNames().ToArray();
+            if (portNames.Length != 0)
+            {
+                PortNamesComboBox.Items.Clear();
+                foreach (var name in portNames)
+                {
+                    PortNamesComboBox.Items.Add(name);
+                }
+
+                PortNamesComboBox.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("COM-порт Arduino не найден",
+                    "Ой...", MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                ConnectPortButton.IsEnabled = false;
+            }
+        }
+
+        private void ConnectPortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_arduino.IsArduinoConnected == false) ConnectArduino();
+            else DisconnectArduino();
+        }
+
+        private void ConnectArduino()
+        {
+            FindPortsButton.IsEnabled = false;
+
+            var port = (string)PortNamesComboBox.SelectedItem;
+            PortNamesComboBox.IsEnabled = false;
+
+            ConnectPortButton.Style = (Style)Resources["ConnectedArduino"];
+            ConnectPortButton.Content = "Arduino подключен";
+
+            FillGrid(_arduino.SubstationCount);
+
+            _arduino.InvasionHappened += StartDialogWindow;
+            _arduino.ConnectToPort(port);
+        }
+
+        private void DisconnectArduino()
+        {
+            FindPortsButton.IsEnabled = true;
+
+            PortNamesComboBox.IsEnabled = true;
+            PortNamesComboBox.SelectedItem = null;
+
+            ConnectPortButton.Style = (Style)Resources["DisconnectedArduino"];
+            ConnectPortButton.Content = "Arduino отключен";
+
+            ClearGrid();
+
+            _arduino.InvasionHappened -= StartDialogWindow;
+            _arduino.DisconnectFromPort();
+        }
+
+        private void StopSignalButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _arduino.DisableSignalling();
+        }
+
+        private void PortNamesComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ConnectPortButton.Style = (Style)Resources["SampleButton"];
+            ConnectPortButton.Content = "Подключиться";
+            ConnectPortButton.IsEnabled = ((ComboBox)sender).SelectedItem != null;
+        }
+
+        private void EnableStation(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var c = button.Name[button.Name.Length - 1];
+            var index = c - '0';
+            _arduino.LightLed(index);
+        }
+
+        private void DisableStation(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var c = button.Name[button.Name.Length - 1];
+            var index = c - '0';
+            _arduino.TurnOffLed(index);
+        }
+
+        private void StartDialogWindow()
+        {
+            var message = "Кажется у нас гости! Включить сигнализацию?";
+            var caption = "Тревога!";
+            var result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                _arduino.EnableSignalling();
+            }
+        }
+
+        #endregion
+
+        #region InnerMethods
+
+        private void FillGrid(int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                AddGridRow(1);
+
+                var buttons = GetSubstationButtons(i);
+                foreach (var button in buttons)
+                {
+                    ConnectedPortGrid.Children.Add(button);
+                }
+            }
+
+            AddGridRow(2);
+            var stopSignallingButton = GetStopSignallingButton(count);
+            ConnectedPortGrid.Children.Add(stopSignallingButton);
+
+            ConnectedPortGrid.Visibility = Visibility.Visible;
+        }
+
+        private Button GetStopSignallingButton(int count)
+        {
+            var stopSignallingButton = new Button
+            {
+                Name = "StopSignalButton",
+                Content = "Отключить сигнализацию",
+                Style = (Style)Resources["SampleButton"],
+                Width = 300,
+                Height = 75
+            };
+            stopSignallingButton.Click += StopSignalButton_OnClick;
+            Grid.SetRow(stopSignallingButton, count);
+            Grid.SetColumn(stopSignallingButton, 0);
+            Grid.SetColumnSpan(stopSignallingButton, 2);
+            return stopSignallingButton;
+        }
+
+        private void AddGridRow(int length)
+        {
+            var row = new RowDefinition
+            {
+                Height = new GridLength(length, GridUnitType.Star)
+            };
+            ConnectedPortGrid.RowDefinitions.Add(row);
+        }
+
+        private IEnumerable<Button> GetSubstationButtons(int number)
+        {
+            var enableButton = new Button
+            {
+                Name = $"EnableSubstation{number + 1}",
+                Content = $"Включить подстанцию №{number + 1}",
+                Style = (Style)Resources["SampleButton"]
+            };
+            enableButton.Click += EnableStation;
+            Grid.SetColumn(enableButton, 0);
+            Grid.SetRow(enableButton, number);
+
+            var disableButton = new Button
+            {
+                Name = $"DisableSubstation{number + 1}",
+                Content = $"Отключить подстанцию №{number + 1}",
+                Style = (Style)Resources["SampleButton"]
+            };
+            disableButton.Click += DisableStation;
+            Grid.SetColumn(disableButton, 1);
+            Grid.SetRow(disableButton, number);
+
+            return new[] { enableButton, disableButton };
+        }
+
+        private void ClearGrid()
+        {
+            ConnectedPortGrid.Children.Clear();
+            ConnectedPortGrid.RowDefinitions.Clear();
+            ConnectedPortGrid.Visibility = Visibility.Collapsed;
+        }
+
+        #endregion
+    }
+}
